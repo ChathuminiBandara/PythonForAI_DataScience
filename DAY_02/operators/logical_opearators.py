34d0fccbf485c7a4198de9f4009bd571a385ddfd
ChathuminiBandara <chathubandara500@gmail.com>
2024-11-12 14:51:14 +0530

    logical operations

---
+++ DAY_02/operators/logical_opearators.py
@@ -0,0 +1,10 @@
+x, y = 5,6
+
+print(x> 1 and y<10)
+print(x> 10 and y<10)
+print("----------------")
+print(x> 1 or y<10)
+print(x> 10 or y<10)
+print("----------------")
+print(not y>10)
+print(not y<10)
---
+++ DAY_02/operators/operators_python.py
@@ -0,0 +1,5 @@
+z = int(input("Input the value: "))  
+if z % 7 == 0:
+    print("The input divides by 7")
+else:
+    print("The input doesn't divide by 7")
