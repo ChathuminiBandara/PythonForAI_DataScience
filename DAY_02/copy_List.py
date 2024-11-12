4c83721d75027dedc52c18d374246d0f5a147e91
ChathuminiBandara <chathubandara500@gmail.com>
2024-11-12 09:14:34 +0530

    list copying ways, copy(), list() constructor

---
+++ DAY_02/copy_List.py
@@ -0,0 +1,12 @@
+my_list_1 = [1, 2, 3, ["a", "b"]]
+my_list_2 = my_list_1.copy()
+
+print(my_list_2)
+
+my_list_1[1] = "KAMAL"
+print(my_list_1)
+print("My_List_3 by copy :" , my_list_2)
+
+
+my_list_3  = list(my_list_1)
+print("My_List_3 by constructor:" , my_list_3)
