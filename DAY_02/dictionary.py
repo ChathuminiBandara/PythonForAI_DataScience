0f1b334f3849e71bf9401619f539fccda4b55b08
ChathuminiBandara <chathubandara500@gmail.com>
2024-11-12 11:36:12 +0530

    Dictionary details

---
+++ DAY_02/dictionary.py
@@ -0,0 +1,9 @@
+thisDict = {
+    "name": "Sugar",
+    "name": "Yam",
+    "Weight": "2Kg",
+    "Price": 130.98
+}
+
+print("Items :" ,thisDict ,"Type : " , type(thisDict) , "Lenght of the Dict : ", len(thisDict))
+print("Accessing a custom key :" , thisDict["Weight"])
