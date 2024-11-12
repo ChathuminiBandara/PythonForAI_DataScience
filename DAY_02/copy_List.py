480bb605f70bf610602c334a31ac7ace8933d729
ChathuminiBandara <chathubandara500@gmail.com>
2024-11-12 09:19:58 +0530

    list joinign by +

--- DAY_02/copy_List.py
+++ DAY_02/copy_List.py
@@ -10,3 +10,7 @@ print("My_List_3 by copy :" , my_list_2)
 
 my_list_3  = list(my_list_1)
 print("My_List_3 by constructor:" , my_list_3)
+
+# joining by +
+my_list_5 = my_list_1 + my_list_3
+print("List joining by +  :",my_list_5)
