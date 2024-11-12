4a427de56c97fb4015deb61d0051858f0e645643
ChathuminiBandara <chathubandara500@gmail.com>
2024-11-12 11:09:39 +0530

    nested list, tuple index ,items,catching

---
+++ DAY_02/nested_List_Tuples.py
@@ -0,0 +1,2 @@
+my_tuple_1 = ((12,10,8),("Dog", "Cat", "Rat"), (True, False))
+print(my_tuple_1[0][1], my_tuple_1[1][2], my_tuple_1[2][0])
---
+++ DAY_02/range.py
@@ -0,0 +1,4 @@
+list_1 = (2,5,8,11,14,17)
+s_1 = range(2, 15, 3)
+print(type(s_1))
+print(s_1[2])
