9df78c15c88769ae5743652b6817254732338a0a
ChathuminiBandara <chathubandara500@gmail.com>
2024-11-19 18:56:05 +0530

    brain teasers

--- DAY_03/function_defenition.py
+++ DAY_03/function_defenition.py
@@ -1,5 +1,9 @@
-def caluculate_sum(num1, num2):
-    sum = num1 + num2
-    return print(sum)
+CONST_PIE = 3.14
 
-caluculate_sum(12,20)
+def caluculate_area(r):
+    area = CONST_PIE*r*r
+    return print(area)
+
+radiaus = int(input("Enter the  radius :"))
+
+caluculate_area(radiaus)
