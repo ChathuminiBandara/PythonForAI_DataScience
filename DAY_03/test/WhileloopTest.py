61ec550fb1c32a577e179a5b6132a4d826d34bb0
ChathuminiBandara <chathubandara500@gmail.com>
2024-11-19 11:38:00 +0530

    while loop usages

---
+++ DAY_03/test/WhileloopTest.py
@@ -0,0 +1,6 @@
+x = int(input("Enter the  number u wanna stop :"))
+y = 2
+while y < x:
+    print(y)
+    y += 2
+print("END")    
---
+++ DAY_03/whileloop.py
@@ -0,0 +1,7 @@
+m = 5
+i = 0
+
+while i<m:
+    print(i, end= " ")
+    i += 1
+print("End")    
