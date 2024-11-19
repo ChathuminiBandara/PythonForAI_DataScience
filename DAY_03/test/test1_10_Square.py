5406ffa24641f713538ac60abe857b8bcaee16ae
ChathuminiBandara <chathubandara500@gmail.com>
2024-11-19 10:41:31 +0530

    loop comprehension

--- DAY_03/test/test1_10_Square.py
+++ DAY_03/test/test1_10_Square.py
@@ -2,6 +2,8 @@
 # print([my**2 for my in my_list ])
 
 
-# --------------
+# -----printin even's square---------
 
-print([x**2 for x in range(1, 11)])
+print([x**2 for x in range(2, 10, 2)])
+
+print([x**2 for x in range(1, 11) if x % 2 == 0])
