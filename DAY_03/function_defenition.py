95ab86fe1cda03312c031c63e128a4edc5773516
ChathuminiBandara <chathubandara500@gmail.com>
2024-11-19 14:27:20 +0530

    function declaring

---
+++ DAY_03/function_defenition.py
@@ -0,0 +1,5 @@
+def caluculate_sum(num1, num2):
+    sum = num1 + num2
+    return print(sum)
+
+caluculate_sum(12,20)
--- DAY_03/merging_Dictionary.py
+++ DAY_03/merging_Dictionary.py
@@ -3,4 +3,9 @@ dict_2 = {"b": 2}
 dict_3 = {"c": 3}
 
 dict_4 = dict_1 | dict_2 | dict_3
-print(dict_4)
+# print(dict_4)
+
+
+# dict_4.get(key, default.value)
+print(dict_4.get("u", "Not Found"))
+
